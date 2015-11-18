db15070b7e0753ff61f010ac9b917807c984ab52
Aneesh Kher <aneeshkher@gmail.com>
2015-11-18 12:12:33 -0500

    Add SMS alerts for excess memory usage and CPU usage

--- alert.js
+++ alert.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var os = require('os');
-var nodemailer = require('nodemailer');
+//var nodemailer = require('nodemailer');
 var fs = require('fs');
+var redis = require('redis');
+
+var client = redis.createClient(6379, '52.34.15.28', {});
 
 var accSID = fs.readFileSync('acc_sid', 'utf8').trim();
 var authToken = fs.readFileSync('auth_token', 'utf8').trim();
@@ -10,7 +13,6 @@ accSID.slice(0, accSID.length - 1);
 authToken.slice(0, authToken.length - 1);
 
 var twiClient = require('twilio')(accSID, authToken);
-console.log(twiClient);
 
 //var transporter = nodemailer.createTransport({
 //    service: 'Gmail',
@@ -35,7 +37,7 @@ function memoryLoad()
 	var total = os.totalmem();
 	var load = os.totalmem() - os.freemem();
 	var percentage = (load/total)*100;
-	return percentage;
+	return percentage.toFixed(2);
 }
 
 function getProcessorUsage () {
@@ -108,6 +110,13 @@ function measureLatenancy(server)
 	return server.latency;
 }
 
+function cpuLoadAll () {
+	var loads = os.loadavg();
+	var percentage = loads[0];
+	percentage = percentage * 100;
+	return percentage.toFixed(2);;
+}
+
 
 var flag = 0;
 setInterval( function () 
@@ -116,29 +125,71 @@ setInterval( function ()
 	//cpuAverage();
 	//console.log(os.loadavg());
 	var memLoad = memoryLoad();
-	if (memLoad > 70) {
-		console.log("ALERT! EXCESS MEMORY USAGE", memLoad, "%");
-		if (flag == 0) {	
-			//transporter.sendMail(mailOptions, function(error, info) {
-    		//	if(error) {
-        	//		return console.log(error);
-    		//	}
-    		//		//console.log('Message sent: ' + info.response);
+	var cpuLoad = cpuLoadAll();
+	console.log("Memory: ", memLoad);
+	console.log("CPU: ", cpuLoad);
 
-			//});
-			twiClient.sendMessage({
-			    body: "EXCESS MEMORY USAGE",
-			    to: "+12526218047",
-			    from: "+13343844530"
-			}, function(err, message) {
-			    //process.stdout.write(message.sid);
-				console.log(err);
-				console.log("-----");
-				//console.log(message);
-			});
-			flag = 1;
-		}
+	if (memLoad > 70) {
+		client.get("memFlag", function(err, value) {
+			if (value == 0) {
+				twiClient.sendMessage({
+			    	body: "ALERT! EXCESS MEMORY USAGE",
+			    	to: "+12526218047",
+			    	from: "+13343844530"
+				}, function(err, message) {
+			    	//process.stdout.write(message.sid);
+					console.log(err);
+					console.log("-----");
+				});
+
+				client.set("memFlag", 1);
+			} else {
+				console.log("ALERT! Excess memory usage. Notified Ops Team.");
+			}
+		});
 	}
+
+	if (cpuLoad > 30) {
+		client.get("cpuFlag", function(err, value) {
+			console.log("Flag value: ", value);
+			if (value == 0) {
+				//console.log("CPU Load too much. Sending alert");
+				twiClient.sendMessage({
+			    	body: "ALERT! EXCESS CPU USAGE",
+			    	to: "+12526218047",
+			    	from: "+13343844530"
+				}, function(err, message) {
+
+				});
+				client.set("cpuFlag", 1);
+			} else {
+				console.log("ALERT! Excess CPU Usage. Notified Ops Team");
+			}
+		});
+	}
+	//if (memLoad > 70) {
+	//	console.log("ALERT! EXCESS MEMORY USAGE", memLoad, "%");
+	//	if (flag == 0) {	
+	//		//transporter.sendMail(mailOptions, function(error, info) {
+    //		//	if(error) {
+    //    	//		return console.log(error);
+    //		//	}
+    //		//		//console.log('Message sent: ' + info.response);
+
+	//		//});
+	//		twiClient.sendMessage({
+	//		    body: "EXCESS MEMORY USAGE",
+	//		    to: "+12526218047",
+	//		    from: "+13343844530"
+	//		}, function(err, message) {
+	//		    //process.stdout.write(message.sid);
+	//			console.log(err);
+	//			console.log("-----");
+	//			//console.log(message);
+	//		});
+	//		flag = 1;
+	//	}
+	//}
 	//getProcessorUsage();
 
 }, 2000);
